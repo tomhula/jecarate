7adac5ffb6c89cc8ae75da014cbdf89d01897935
Trup10ka <lukys3036@gmail.com>
2025-01-26 23:43:40 +0100

    Implement auto-redirect to login on '/'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import '@/app/ui/global.css';
-import React from 'react';
-import { Metadata } from 'next';
+import '@/app/ui/global.css'
+import React from 'react'
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: {
-    template: '%s | Jecarate',
-    default: 'Jecarate',
-  },
-  description: 'Jecna rating web app for our food in canteen.',
-};
+    title: {
+        template: '%s | Jecarate',
+        default: 'Jecarate'
+    },
+    description: 'Jecna rating web app for our food in canteen.',
+}
 
 export default function RootLayout(
-    { children }: { children: React.ReactNode; }
-) {
-  return (
-    <html lang="en">
-    <body>{children}</body>
-    </html>
-  );
+    { children }: { children: React.ReactNode }
+)
+{
+    return (
+        <html lang="en">
+        <body>{children}</body>
+        </html>
+    )
 }
