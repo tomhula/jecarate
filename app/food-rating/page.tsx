e8a7e24caae07e5b3ba6fdea4241cd13bced600f
Lukáš Friedl <lukys3036@gmail.com>
2025-01-27 17:40:09 +0100

    Remove wrapping div in FoodSelector

--- app/food-rating/page.tsx
+++ app/food-rating/page.tsx
@@ -3,8 +3,6 @@ import FoodSelector from '@/app/ui/food-rating/food-selector';
 export default function Page()
 {
     return (
-        <div>
-            <FoodSelector />
-        </div>
+        <FoodSelector />
     );
 }
