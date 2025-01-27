8a8ad0468331d07473cee32ee10db25ead0b01a4
Lukáš Friedl <lukys3036@gmail.com>
2025-01-27 17:40:44 +0100

    Add utility styles for components

--- app/ui/util/clickable-div.tsx
+++ app/ui/util/clickable-div.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import React from "react";
+import styles from '../util.module.css'
 
 export function ClickableDiv(props: { children: React.ReactNode, action: () => void })
 {
     return (
-        <div onClick={ props.action } className="cursor-pointer">
+        <div onClick={ props.action } className={`${styles.cursorPointer} ${styles.roundDivButton}`}>
             { props.children }
         </div>
     )
