105d968e341185728b6f387dd725d323e25978ef
Trup10ka <lukys3036@gmail.com>
2025-01-27 18:42:58 +0100

    Add utility interface for answers object

---
+++ app/lib/util.ts
@@ -0,0 +1,9 @@
+interface FormAnswer
+{
+    ration: number | null,
+    taste: number | null,
+    price: number | null,
+    temperature: number | null,
+    looks: number | null,
+    [key: string]: number | null
+}
