c22ec91bce165b6ee0fe79c5d67b7b778d3acc6a
Tomas Hula <tomashula06@gmail.com>
2025-02-23 11:26:09 +0100

    Make "Ohodnotit dnešní oběd" button work

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function Dashboard()
     return (
         <>
             <div className={dashboardStyles.dashboardHeader}>Katalog objedů</div>
-            <button className={dashboardStyles.rateButton}>Ohodnotit dnešní objed</button>
+            <button className={dashboardStyles.rateButton} onClick={() => window.location.href = '/food-rating'}>Ohodnotit dnešní objed</button>
             <div className={dashboardStyles.searchContainer}>
                 <input type="text" placeholder="Hledat podle názvu.." id="search-name"/>
                 <input type="date" id="search-date"/>
