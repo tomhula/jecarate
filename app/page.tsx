f818b6d7bc2082f8a2c4198a1ece0e4445e5edec
Trup10ka <lukys3036@gmail.com>
2025-02-23 05:07:21 +0100

    Add pulsing image for root page simulating loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 'use client'
 
 import { redirect } from 'next/navigation'
-import { authorize } from "@/app/index";
-import { useEffect } from "react";
+import { authorize } from "@/app/index"
+import { useEffect } from "react"
+import Image from "next/image"
+import styles from "@/app/ui/root.module.css"
 
 export default function Page()
 {
     useEffect(() =>
     {
-        if (!authorize())
-            redirect('/login')
-        
-        redirect('/dashboard')
+        setTimeout(() => {
+            if (!authorize())
+                redirect('/login')
+
+            redirect('/dashboard')
+        }, 2000)
     }, []);
     
+    return (
+        <div className={styles.root}>
+            <Image src="/jecarate-icon.png" width={741} height={474} alt="Microsoft Logo"
+                   className={ styles.pulse }/>
+        </div>
+    )
 }
