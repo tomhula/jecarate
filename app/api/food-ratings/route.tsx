2aa80f9c98e5bc5a42bd61673926f6e36a2eaa35
Trup10ka <lukys3036@gmail.com>
2025-02-24 18:10:17 +0100

    Implement uploading rating of lunch

--- app/api/food-ratings/route.tsx
+++ app/api/food-ratings/route.tsx
@@ -1,5 +1,6 @@
 import { query } from "@/app/lib/db.server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { FormAnswer } from "@/app/lib/util";
 
 export async function GET()
 {
@@ -14,4 +15,44 @@ export async function GET()
     }
 }
 
+export async function POST(req: NextRequest)
+{
+    const { foodId, answers } = await req.json();
+    if (!foodId || !answers)
+        return NextResponse.json({ message: "Invalid request" }, { status: 400 });
 
+    console.log(answers);
+    console.log(foodId);
+
+    const answerKeys = Object.keys(answers);
+    if (answerKeys.length !== 5 || !answerKeys.every(key => typeof answers[key] === 'number'))
+        return NextResponse.json({ message: "Invalid answers" }, { status: 400 });
+
+    // Remove beverage from the foodId
+    const foodDbId = await findFoodIdOrCreate(foodId.split(',').slice(0, -1).join(', ').trim())
+    const averageRating = Object.values(answers).reduce((sum: number, value) => sum + (value as number), 0) / answerKeys.length;
+    try
+    {
+        await query(
+            "INSERT INTO lunch_rating (food_id, rating, lunch_date) VALUES (?, ?, CURDATE())",
+            [foodDbId, averageRating]
+        );
+        return NextResponse.json({ message: "Rating submitted successfully" });
+    }
+    catch (error)
+    {
+        return NextResponse.json({ message: "Error saving rating" }, { status: 500 });
+    }
+}
+
+async function findFoodIdOrCreate(foodId: string): Promise<string>
+{
+    const [queryResult, _] = await query("SELECT id FROM food WHERE name = ?", [foodId]) as any[];
+    if (queryResult.length === 0)
+    {
+        await query("INSERT INTO food (name) VALUES (?)", [foodId]);
+        const [newFood, _] = await query("SELECT id FROM food WHERE name = ?", [foodId]) as any[];
+        return newFood[0].id;
+    }
+    return queryResult[0].id;
+}
--- app/ui/food-rating/food-selector.tsx
+++ app/ui/food-rating/food-selector.tsx
@@ -3,25 +3,26 @@
 import { useEffect, useState } from 'react'
 import { FoodOption } from '@/app/ui/food-rating/food-option'
 import { FoodFormQuestion } from '@/app/ui/food-rating/food-question'
-import { FoodFormQuestionAnswerContainer } from "@/app/ui/food-rating/form-answer-container";
+import { FoodFormQuestionAnswerContainer } from "@/app/ui/food-rating/form-answer-container"
 import foodFormStyles from './food-rating.module.css'
-import RatingSlider from "@/app/ui/food-rating/rating-slider";
-import { authorize } from "@/app";
-import { FormAnswer } from "@/app/lib/util";
-import {DayMenu, MenuItem} from "@/app/lib/canteen/CanteenMenuParser";
+import RatingSlider from "@/app/ui/food-rating/rating-slider"
+import { authorize } from "@/app"
+import { FormAnswer } from "@/app/lib/util"
+import { DayMenu, MenuItem } from "@/app/lib/canteen/CanteenMenuParser"
+import { showAlertBubble } from "@/app/ui/util/util"
 
 export default function FoodSelector()
 {
-    const [showForm, setShowForm] = useState(false);
-    const [selectedId, setSelectedId] = useState<string>('');
-    const [menuItems, setMenuItems] = useState<MenuItem[] | null>(null); // To store fetched menu items as objects, initially null
+    const [showForm, setShowForm] = useState(false)
+    const [selectedId, setSelectedId] = useState<string>('')
+    const [menuItems, setMenuItems] = useState<MenuItem[] | null>(null) // To store fetched menu items as objects, initially null
 
     useEffect(() =>
     {
         if (!authorize())
         {
-            window.location.href = '/login';
-            return;
+            window.location.href = '/login'
+            return
         }
 
         fetch('/api/todays-menu')
@@ -29,29 +30,29 @@ export default function FoodSelector()
             {
                 if (!response.ok)
                 {
-                    return { items: [] };
+                    return { items: [] }
                 }
-                return response.json();
+                return response.json()
             })
             .then((data: DayMenu) =>
             {
-                setMenuItems(data.items); // Assumes the response includes { items: MenuItem[] }
+                setMenuItems(data.items) // Assumes the response includes { items: MenuItem[] }
             })
             .catch(error =>
             {
-                console.error("Error fetching menu:", error);
-            });
-    }, []);
+                console.error("Error fetching menu:", error)
+            })
+    }, [])
 
     const handleSelect = (id: string) =>
     {
-        setSelectedId(id);
-    };
+        setSelectedId(id)
+    }
 
     const handleFoodOptionClick = (id: string) =>
     {
-        setShowForm(true);
-        handleSelect(id);
+        setShowForm(true)
+        handleSelect(id)
     }
 
     const chosenAnswers: FormAnswer = {
@@ -61,91 +62,131 @@ export default function FoodSelector()
         temperature: null,
         looks: null
     }
-    
+
     function ratingLabelMap(rating: number)
     {
         switch (rating)
         {
             case 0:
-                return "Určitě ne";
+                return "Určitě ne"
             case 1:
-                return "Spíš ne";
+                return "Spíš ne"
             case 2:
-                return "Ano";
+                return "Ano"
             case 3:
-                return "Určitě ano";
+                return "Určitě ano"
             default:
-                return "";
+                return ""
         }
     }
 
+    function postAnswers(answers: FormAnswer)
+    {
+        fetch('/api/food-ratings', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                foodId: selectedId,
+                answers: answers
+            })
+        })
+            .then(response =>
+            {
+                if (!response.ok)
+                {
+                    throw new Error("Failed to submit food rating")
+                }
+                return response.json()
+            })
+            .then(data =>
+            {
+                showAlertBubble("success", "Food rating submitted!")
+                setShowForm(false)
+                setSelectedId('')
+            })
+            .catch(_ =>
+            {
+                showAlertBubble("error", "Failed to submit food rating")
+            })
+    }
+
     return (
-        <div className={'page-container'}>
-            <div className={foodFormStyles.foodOptions}>
+        <div className={ 'page-container' }>
+            <div className={ foodFormStyles.foodOptions }>
                 { menuItems != null ? (
                     menuItems.length == 0 ?
                         <div>No menu for today</div>
-                    :
+                        :
                         menuItems.map((item) => (
                             <FoodOption
-                                key={item.number}
-                                id={item.number.toString()}
-                                selectedId={selectedId}
-                                onClick={() => handleFoodOptionClick(item.number.toString())}
+                                key={ item.number }
+                                id={ item.description }
+                                selectedId={ selectedId }
+                                onClick={ () => handleFoodOptionClick(item.description) }
                             >
-                                {item.description}
+                                { item.description }
                             </FoodOption>
                         ))
                 ) : (
                     <div>Loading menu...</div>
-                )}
+                ) }
             </div>
 
             { !showForm && (
-                    <div id="message" className={foodFormStyles.message}>
-                        Choose a food
-                    </div>
-                )
+                <div id="message" className={ foodFormStyles.message }>
+                    Choose a food
+                </div>
+            )
             }
 
             { showForm && (
-                <form id="food-form" method="post">
+                <form id="food-form">
                     <FoodFormQuestion>
-                        <label className={foodFormStyles.questionLabel}>Byla porce uspokojivá?</label>
-                            <RatingSlider labelMap={ratingLabelMap} onChange={ val => chosenAnswers.ration = val }></RatingSlider>
+                        <label className={ foodFormStyles.questionLabel }>Byla porce uspokojivá?</label>
+                        <RatingSlider labelMap={ ratingLabelMap }
+                                      onChange={ val => chosenAnswers.ration = val }></RatingSlider>
                     </FoodFormQuestion>
 
                     <FoodFormQuestion>
-                        <label className={foodFormStyles.questionLabel}>Bylo jídlo chutné?</label>
+                        <label className={ foodFormStyles.questionLabel }>Bylo jídlo chutné?</label>
                         <FoodFormQuestionAnswerContainer>
-                            <RatingSlider labelMap={ratingLabelMap} onChange={ val => chosenAnswers.taste = val }></RatingSlider>
+                            <RatingSlider labelMap={ ratingLabelMap }
+                                          onChange={ val => chosenAnswers.taste = val }></RatingSlider>
                         </FoodFormQuestionAnswerContainer>
                     </FoodFormQuestion>
 
                     <FoodFormQuestion>
-                        <label className={foodFormStyles.questionLabel}>Byli by jste ochotni, si za toto jídlo připlatit?</label>
+                        <label className={ foodFormStyles.questionLabel }>Byli by jste ochotni, si za toto jídlo
+                            připlatit?</label>
                         <FoodFormQuestionAnswerContainer>
-                            <RatingSlider labelMap={ratingLabelMap} onChange={ val => chosenAnswers.price = val }></RatingSlider>
+                            <RatingSlider labelMap={ ratingLabelMap }
+                                          onChange={ val => chosenAnswers.price = val }></RatingSlider>
                         </FoodFormQuestionAnswerContainer>
                     </FoodFormQuestion>
 
                     <FoodFormQuestion>
-                        <label className={foodFormStyles.questionLabel}>Mělo jídlo správnou teplotu?</label>
+                        <label className={ foodFormStyles.questionLabel }>Mělo jídlo správnou teplotu?</label>
                         <FoodFormQuestionAnswerContainer>
-                            <RatingSlider labelMap={ratingLabelMap} onChange={ val => chosenAnswers.temperature = val }></RatingSlider>
+                            <RatingSlider labelMap={ ratingLabelMap }
+                                          onChange={ val => chosenAnswers.temperature = val }></RatingSlider>
                         </FoodFormQuestionAnswerContainer>
                     </FoodFormQuestion>
 
                     <FoodFormQuestion>
-                        <label className={foodFormStyles.questionLabel}>Vypadala prezentace jídla chutně ?</label>
+                        <label className={ foodFormStyles.questionLabel }>Vypadala prezentace jídla chutně ?</label>
                         <FoodFormQuestionAnswerContainer>
-                            <RatingSlider labelMap={ratingLabelMap} onChange={ val => chosenAnswers.looks = val }></RatingSlider>
+                            <RatingSlider labelMap={ ratingLabelMap }
+                                          onChange={ val => chosenAnswers.looks = val }></RatingSlider>
                         </FoodFormQuestionAnswerContainer>
                     </FoodFormQuestion>
 
-                    <button type="submit" className={foodFormStyles.submitButton}>Submit</button>
+                    <button type="button" onClick={ () => postAnswers(chosenAnswers) }
+                            className={ foodFormStyles.submitButton }>Submit
+                    </button>
                 </form>
-            )}
+            ) }
         </div>
-    );
+    )
 }
