b1af486efb01d5691cfb6476abc43eed6cd50bcb
Trup10ka <lukys3036@gmail.com>
2025-02-25 23:51:35 +0100

    Implement star representation of rating

--- app/ui/dashboard/lunch-card.tsx
+++ app/ui/dashboard/lunch-card.tsx
@@ -10,11 +10,16 @@ interface LunchCardProps
 
 export default function LunchCard({name, date, rating}: LunchCardProps)
 {
+    const maxRating = 3
     return (
         <div className={styles.lunchCard}>
             <h3 className={styles.lunchCardName}>{name}</h3>
             <div className={styles.lunchCardFieldName}>Date: <p>{date}</p></div>
             <div className={styles.lunchCardFieldName}>Rating: <p>{rating}</p></div>
+            <div className={styles.ratingContainer}>
+                <div className={styles.stars}>★★★</div>
+                <div className={styles.starsFull} style={{width: `${(rating / maxRating) * 100}%`}}>★★★</div>
+            </div>
         </div>
     )
 }
